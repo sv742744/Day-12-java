1286458e08766abd78b8daf8e02823fb122aed4f
sv742744 <shashi09op@gmail.com>
2024-12-13 15:57:11 +0530

    Add files via upload

---
+++ union1.java
@@ -0,0 +1,36 @@
+/*
+3
+1 2 3
+3 
+3 2 5
+[1, 2, 3]
+[2, 3, 5]
+[1, 2, 3, 5]
+*/
+import java.util.*;
+class union1{
+    public static void main(String [] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr1[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr1[i]=s.nextInt();
+        }
+         int m=s.nextInt();
+        int arr2[]=new int[n];
+        for(int i=0;i<m;i++){
+            arr2[i]=s.nextInt();
+        }
+        Set<Integer> a=new HashSet<>();
+        Set<Integer> b=new HashSet<>();
+        for(int s1:arr1)
+            a.add(s1);
+        for(int s2:arr2)
+            b.add(s2);
+        System.out.println(a);
+        System.out.println(b);
+        Set<Integer> c=new HashSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
+    }
+}
