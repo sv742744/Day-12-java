30e6d8577886508382ab88214a392285d87e72ba
sv742744 <shashi09op@gmail.com>
2024-12-13 15:55:30 +0530

    Add files via upload

---
+++ intersection1.java
@@ -0,0 +1,35 @@
+/*
+3
+1 2 3 
+3 3 2 5  
+[1, 2, 3]
+[2, 3, 5]
+[2, 3]
+*/
+import java.util.*;
+class intersection1{
+    public static void main(String [] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr1[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr1[i]=s.nextInt();
+        }
+         int m=s.nextInt();
+        int arr2[]=new int[n];
+        for(int i=0;i<m;i++){
+            arr2[i]=s.nextInt();
+        }
+        Set<Integer> a=new HashSet<>();
+        Set<Integer> b=new HashSet<>();
+        for(int s1:arr1)
+            a.add(s1);
+        for(int s2:arr2)
+            b.add(s2);
+        System.out.println(a);
+        System.out.println(b);
+        Set<Integer> c=new HashSet<>(a);
+        c.retainAll(b);
+        System.out.println(c);
+    }
+}
